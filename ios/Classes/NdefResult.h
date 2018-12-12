5f08df7af5f344492c06ef59613a63f0877168fc
Lee Higgitt <lee.higgitt@gmail.com>
2018-12-12 14:10:52 +0000

    Update NdefResult.h

--- ios/Classes/NdefResult.h
+++ ios/Classes/NdefResult.h
@@ -10,9 +10,9 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NdefResult : NSObject
-@property (assign, nonatomic) NSString *data;
-@property (assign, nonatomic) NSString *scheme;
-@property (assign, nonatomic) NSString *language;
+@property (retain) NSString *data;
+@property (retain) NSString *scheme;
+@property (retain) NSString *language;
 
 -(id) initWithData:(NSString *)data andScheme:(NSString *)scheme;
 -(id) initWithData:(NSString *)data andLanguage:(NSString *)language;
